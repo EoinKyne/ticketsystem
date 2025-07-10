e1f4af0c454bc3892ce1ed247d11deed479f7ad4
EoinKyne <eoin_kyne@yahoo.com>
2025-07-10 16:55:09 +0100

    updated schema for foreign key

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,16 @@ CREATE TABLE IF NOT EXISTS Ticket (
     id UUID NOT NULL,
     location varchar(50) NOT NULL,
     ticket_creation_date_time timestamp,
+    product varchar(50) NOT NULL,
+    destination varchar (50) NOT NULL,
+    version INT,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Weight_Ticket (
+    id UUID NOT NULL,
+    ticket_number UUID NOT NULL REFERENCES Ticket(id),
     gross_weight double precision,
     ticket_gross_weight_date_time timestamp,
     tare_weight double precision,
@@ -10,3 +20,5 @@ CREATE TABLE IF NOT EXISTS Ticket (
     version INT,
     PRIMARY KEY (id)
 );
+
+
